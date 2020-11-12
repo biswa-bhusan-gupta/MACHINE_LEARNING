23be84bbb3b22a08426bed9e08ac39fefd391d27
Biswa Bhusan Gupta <66106600+biswabhusangupta@users.noreply.github.com>
2020-11-12 10:16:42 +0530

    Add files via upload

---
+++ PYTHON/12.Import_Module.py
@@ -0,0 +1,8 @@
+from Export_Module import myFunc
+
+from Main_Package import Main_Script
+from Main_Package.Sub_Package import Sub_Script
+
+myFunc()
+Main_Script.main_report()
+Sub_Script.sub_report()
---
+++ PYTHON/13.Two.py
@@ -0,0 +1,11 @@
+# from One import Func
+# Func();
+
+import One
+One.Func();
+
+print("I am at Two")
+if __name__ == "__main__":
+    print("Two is not Imported");
+else:
+    print("Two is Imported");
